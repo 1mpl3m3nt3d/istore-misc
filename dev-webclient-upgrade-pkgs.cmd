dc607c69cc1d9f0bd7e8cf89452b719d5b7952cb
1mpl3m3nt3d <105541001+1mpl3m3nt3d@users.noreply.github.com>
2022-09-07 01:03:06 +0300

    Up

--- dev-webclient-upgrade-pkgs.cmd
+++ dev-webclient-upgrade-pkgs.cmd
@@ -7,9 +7,23 @@ cd ..
 cd istore-webclient
 
 echo.
+call npm --global update
+
+echo.
 call npx --yes npm-check-updates@latest --target latest --pre 1 --upgrade --color ^
 --concurrency 8 --configFilePath . --configFileName .ncurc.json --cwd . ^
 --dep prod,dev,bundle,optional,peer --errorLevel 1
+
+echo.
+call yarn install
+echo.
+call yarn up
+echo.
+call yarn
+echo.
+call yarn dlx @yarnpkg/sdks vscode
+echo.
+call yarn
 
 echo.
 pause
